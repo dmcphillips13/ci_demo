98b3fa7a9a629d57847ad2a197c9c7deba9e1c64
Denis McPhillips <denismcphillips@gmail.com>
2020-04-23 19:30:48 -0400

    separate files

---
+++ db.js
@@ -0,0 +1,14 @@
+const Sequelize = require("sequelize");
+const { STRING } = Sequelize;
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_db_test"
+);
+
+const User = conn.define("user", {
+  name: STRING,
+});
+
+module.exports = {
+  User,
+  conn,
+};
